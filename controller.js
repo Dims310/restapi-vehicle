a88c7b945242d8e34eba4b34986f741ef8c03c9a
Dims310 <dimasdwisuprayogi@gmail.com>
2023-04-14 14:42:00 +0700

    fix bad comments, add patch methods

--- controller.js
+++ controller.js
@@ -7,7 +7,7 @@ exports.index = function(req, res){
     response.ok("REST API Vehicle", res)
 }
 
-// get all vehicles data
+// get all users data
 // output: {status, values: {id, name, created_at, updated_at}}
 exports.getAllUser = function(req, res){
     connection.query("SELECT * FROM users", (err, rows, fields) => {
@@ -16,13 +16,34 @@ exports.getAllUser = function(req, res){
     });
 }
 
-// add vehicles data
+// get all vehicles data
+// output: {status, values: {id, name, created_at, updated_at}}
+exports.getAllVehicleBrand = function(req, res){
+    connection.query("SELECT * FROM vehicle_brand", (err, rows, fields) => {
+        if (err) throw err;
+        response.ok(rows, res)
+    });
+}
+
+// add vehicle brand data *POST
 // output: {status, values}
 exports.postVehicles = function(req, res){
     var name = req.body.name;
     connection.query("INSERT INTO vehicle_brand (name) VALUES (?)", [name],
+    function(err, rows, fields){ 
+        if (err) throw err;
+        response.ok("Success Create Vehicle Brand", res);
+    });
+}
+
+// change data according ID vehicle brand
+// output: {status, values}
+exports.patchVehicles = function(req, res){
+    var id = req.body.id;
+    var name = req.body.name;
+    connection.query("UPDATE vehicle_brand SET name=?, current_timestamp() WHERE id=?", [name, id],
     function(err, rows, fields){
         if (err) throw err;
-        response.ok("Success create brand vehicle", res);
+        response.ok("Success Update Vehicle Brand", res);
     });
 }

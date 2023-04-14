ffc4e483bade74f72096a17d2d50b0eee1cd8300
Dims310 <dimasdwisuprayogi@gmail.com>
2023-04-14 13:41:30 +0700

    get all user data

--- controller.js
+++ controller.js
@@ -6,3 +6,11 @@ var connection = require('./connection');
 exports.index = function(req, res){
     response.ok("REST API Vehicle", res)
 }
+
+// get all vehicles data
+exports.getAllUser = function(req, res){
+    connection.query("SELECT * FROM users", (err, rows, fields) => {
+        if (err) throw err;
+        response.ok(rows, res)
+    });
+}

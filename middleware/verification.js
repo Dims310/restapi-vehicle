8cdbacbae94eecc1cb561f6a8c72ced26759a71c
Dims310 <dimasdwisuprayogi@gmail.com>
2023-04-16 11:43:45 +0700

    use the verification each route

--- middleware/verification.js
+++ middleware/verification.js
@@ -3,7 +3,7 @@ const response = require('../res');
 const config = require('../config/secret');
 const connection = require('../connection');
 
-function verification(){
+function verification(need_admin){
     return function(req, res, next){
         var tokenWithBearer = req.headers.authorization;
         if (tokenWithBearer){
@@ -14,14 +14,19 @@ function verification(){
                         return response.bad('Unexpected Token.', res);
                     });
                 } else {
-                    connection.query("SELECT users.is_admin, token FROM access_token JOIN users WHERE access_token.user_id = users.id AND token=?", [token], (err, rows) => {
-                        if (rows[0].is_admin == 1){
-                            req.auth = decoded;
-                            next();
-                        } else {
-                            return response.bad('Unauthorized User as Admin, Try Again.', res);
-                        }
-                    });
+                    if (need_admin == 1){ // if need admin to get, post, patch, delete data
+                        connection.query("SELECT users.is_admin, token FROM access_token JOIN users WHERE access_token.user_id = users.id AND token=?", [token], (err, rows) => {
+                            if (rows[0].is_admin == 1){
+                                req.auth = decoded;
+                                next();
+                            } else {
+                                return response.bad('Unauthorized User as Admin, Try Again.', res);
+                            }
+                        });
+                    } else { // else the routes does not need admin
+                        req.auth = decoded;
+                        next();
+                    }
                 }
             })
         } else {
